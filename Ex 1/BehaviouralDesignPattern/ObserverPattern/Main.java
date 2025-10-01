9c5660f7841229a4543f0c7b05e6209c880d8434
karthiga-ravi <128077856+karthiga-ravi@users.noreply.github.com>
2025-10-01 19:41:38 +0530

    Update Main.java

--- Ex 1/BehaviouralDesignPattern/ObserverPattern/Main.java
+++ Ex 1/BehaviouralDesignPattern/ObserverPattern/Main.java
@@ -2,31 +2,46 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Create the notifier (subject)
         OrderNotifier notifier = new OrderNotifier();
 
+        // Add observers
         notifier.addObserver(new CoffeeMakerObserver());
         notifier.addObserver(new CustomerObserver());
 
-        notifier.setOrderReady("Order #101");
+        // Get order input from user
+        System.out.print("Enter order name: ");
+        String order = sc.nextLine();
+
+        // Notify observers
+        notifier.setOrderReady(order);
+
+        sc.close();
     }
 }
 
+// Observer interface
 interface Observer {
     void notify(String message);
 }
 
+// Concrete observer: CoffeeMaker
 class CoffeeMakerObserver implements Observer {
     public void notify(String msg) {
         System.out.println("CoffeeMaker notified: " + msg);
     }
 }
 
+// Concrete observer: Customer
 class CustomerObserver implements Observer {
     public void notify(String msg) {
         System.out.println("Customer notified: " + msg);
     }
 }
 
+// Subject / Notifier
 class OrderNotifier {
     private List<Observer> observers = new ArrayList<>();
 
