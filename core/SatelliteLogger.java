4df1e2b860645ee3551d42c30801a14357b0914d
karthiga-ravi <128077856+karthiga-ravi@users.noreply.github.com>
2025-10-01 12:44:40 +0530

    Delete core directory

--- core/SatelliteLogger.java
+++
@@ -1,9 +0,0 @@
-package SatelliteSystem.core;
-
-import java.time.LocalDateTime;
-
-public class SatelliteLogger {
-    public void log(String message) {
-        System.out.println(LocalDateTime.now() + " [LOG] " + message);
-    }
-}
